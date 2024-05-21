40c1d58c60d1d20bbc10006c8f52ccc3be54da0a
chintan-harkhani33 <chintan.harkhani@ghanshyamdigital.com>
2024-05-21 15:34:24 +0530

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ssh ubuntu@ec2-13-201-103-255.ap-south-1.compute.amazonaws.com -i "../my-nest-api.pem" << EOF
-ssh -i "../my-nest-api.pem" ubuntu@ec2-13-201-103-255.ap-south-1.compute.amazonaws.com
+ssh -i "../my-nest-api.pem" ubuntu@ec2-13-127-10-215.ap-south-1.compute.amazonaws.com
 sudo su
 # cd /var/www/html/dtm_node_backend
 # eval \$(ssh-agent)
